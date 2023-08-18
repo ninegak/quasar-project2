33ef15f07724b4a2c51e30cc3ddb2834ab34c4e8
ninegak <zxce2546@gmail.com>
2023-08-18 15:25:16 +0700

    we added script

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,5 @@
 <template>
-<!--
-Forked from:
-https://quasar.dev/vue-components/form/#example--basic
--->
-<div id="q-app" style="min-height: 100vh;">
-<div class="q-pa-md" style="max-width: 400px">
+  <div class="q-pa-md" style="max-width: 400px">
 
     <q-form
       @submit="onSubmit"
@@ -18,7 +13,7 @@ https://quasar.dev/vue-components/form/#example--basic
         hint="Name and surname"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
-      ></q-input>
+      />
 
       <q-input
         filled
@@ -30,25 +25,24 @@ https://quasar.dev/vue-components/form/#example--basic
           val => val !== null && val !== '' || 'Please type your age',
           val => val > 0 && val < 100 || 'Please type a real age'
         ]"
-      ></q-input>
+      />
 
-      <q-toggle v-model="accept" label="I accept the license and terms"></q-toggle>
+      <q-toggle v-model="accept" label="I accept the license and terms" />
 
       <div>
-        <q-btn label="Submit" type="submit" color="primary"></q-btn>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"></q-btn>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
 
   </div>
-</div>
 </template>
 
 <script>
-const { useQuasar } = Quasar
-const { ref } = Vue
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
 
-const app = Vue.createApp({
+export default {
   setup () {
     const $q = useQuasar()
 
@@ -87,14 +81,5 @@ const app = Vue.createApp({
       }
     }
   }
-})
-
-app.use(Quasar, { config: {} })
-app.mount('#q-app')
-
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+}
 </script>
